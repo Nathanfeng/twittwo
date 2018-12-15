4d6f701f776e18accbea319e64d69c9ba2d1e4af
Nathan Feng <nathanlfeng@gmail.com>
2018-12-15 14:56:22 -0500

    added the signin

--- server/handlers/auth.js
+++ server/handlers/auth.js
@@ -1,7 +1,44 @@
 const db = require('../models/index');
 const jwt = require('jsonwebtoken');
 
-exports.signin = function(){}
+exports.signin = async function(req, res, next){
+  try {
+    let user = await db.User.findOne({
+      email: req.body.email
+    });
+    let {id, username, profileImageUrl} = user;
+    let isMatch = await user.comparePassword(req.body.password);
+
+    if(isMatch){
+        let token = jwt.sign({
+          id,
+          username,
+          profileImageUrl
+        },
+        process.env.SECRET_KEY
+      );
+      return res.status(200).json({
+        id,
+        username,
+        profileImageUrl,
+        token
+      });
+    } else {
+      return next({
+        status: 400,
+        message: 'Invalid email or password!'
+      })
+
+    }
+
+  } catch (e) {
+    return next({
+      status: 400,
+      message: "Invalid email or password!!"
+    })
+  }
+
+}
 
 exports.signup = async function( req, res, next){
   try {
